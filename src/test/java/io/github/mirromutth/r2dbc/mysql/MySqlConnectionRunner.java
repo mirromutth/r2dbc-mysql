40dffcb1f01b6c4e116d8b15245d9ba3fa47bea5
Mirro Mutth <mirromutth@gmail.com>
2019-07-21 01:39:26 +0800

    Improve unit tests runner
    Change `MySqlConnectionRunner` to `final class` because functional interface is useless for running unit tests in multiple MySQL versions.

--- src/test/java/io/github/mirromutth/r2dbc/mysql/MySqlConnectionRunner.java
+++ src/test/java/io/github/mirromutth/r2dbc/mysql/MySqlConnectionRunner.java
@@ -20,9 +20,7 @@ import org.reactivestreams.Publisher;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.time.Duration;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Function;
 
@@ -30,43 +28,47 @@ import java.util.function.Function;
  * Simple to execute a task in the newly connection, any exceptions in the
  * execution will be thrown, connection will always be closed after use.
  */
-@FunctionalInterface
-public interface MySqlConnectionRunner {
+final class MySqlConnectionRunner {
 
-    MySqlConnectionRunner[] ALL_RUNNER = {
-        MySqlConnectionRunner.ofVersion("5_6", false), // MySQL 5.6.x community version without SSL.
-        MySqlConnectionRunner.ofVersion("5_7", true) // MySQL 5.7.x community version with SSL.
+    private static final MySqlConnectionRunner[] ALL_RUNNER = {
+        new MySqlConnectionRunner("5_6", false), // MySQL 5.6.x community version without SSL.
+        new MySqlConnectionRunner("5_7", true) // MySQL 5.7.x community version with SSL.
     };
 
-    void run(Function<MySqlConnection, Publisher<?>> consumer) throws Throwable;
+    private final String version;
+
+    private final boolean ssl;
+
+    private MySqlConnectionRunner(String version, boolean ssl) {
+        this.version = version;
+        this.ssl = ssl;
+    }
+
+    private void run(Function<MySqlConnection, Publisher<?>> consumer) throws Throwable {
+        CountDownLatch latch = new CountDownLatch(1);
+        AtomicReference<Throwable> cause = new AtomicReference<>();
+
+        MySQLHelper.getFactoryByVersion(version, ssl).create()
+            .flatMap(connection -> Flux.from(consumer.apply(connection))
+                .onErrorResume(e -> connection.close().then(Mono.error(e)))
+                .concatWith(connection.close().then(Mono.empty()))
+                .then())
+            .subscribe(null, e -> {
+                cause.set(e);
+                latch.countDown();
+            }, latch::countDown);
+
+        latch.await();
+
+        Throwable e = cause.get();
+        if (e != null) {
+            throw e;
+        }
+    }
 
     static void runAll(Function<MySqlConnection, Publisher<?>> consumer) throws Throwable {
         for (MySqlConnectionRunner runner : ALL_RUNNER) {
             runner.run(consumer);
         }
-    }
-
-    static MySqlConnectionRunner ofVersion(String version, boolean ssl) {
-        return (consumer) -> {
-            CountDownLatch latch = new CountDownLatch(1);
-            AtomicReference<Throwable> cause = new AtomicReference<>();
-
-            MySQLHelper.getFactoryByVersion(version, ssl).create()
-                .flatMap(connection -> Flux.from(consumer.apply(connection))
-                    .onErrorResume(e -> connection.close().then(Mono.error(e)))
-                    .concatWith(connection.close().then(Mono.empty()))
-                    .then())
-                .subscribe(null, e -> {
-                    cause.set(e);
-                    latch.countDown();
-                }, latch::countDown);
-
-            latch.await();
-
-            Throwable e = cause.get();
-            if (e != null) {
-                throw e;
-            }
-        };
     }
 }
