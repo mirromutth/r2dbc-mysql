b797cb3764117efa81020e1c6b050e7339b652bf
Mirro Mutth <mirromutth@gmail.com>
2019-09-11 17:21:55 +0800

    Polishing column names search

--- src/main/java/io/github/mirromutth/r2dbc/mysql/MySqlColumnNames.java
+++ src/main/java/io/github/mirromutth/r2dbc/mysql/MySqlColumnNames.java
@@ -26,39 +26,31 @@ package io.github.mirromutth.r2dbc.mysql;
  * <li>Search: find with case sensitive first, then find with case insensitive
  * when not found in case sensitive.</li>
  * </ul>
- *
  * <p>
  * For example:
  * Sort first: abc AB a Abc Ab ABC A ab b B -> A a B b AB Ab ab ABC Abc abc
  * Then find "aB" use the same compare rule,
  *
- * @see #compare(CharSequence, CharSequence)
+ * @see #compare(String, String)
  */
 final class MySqlColumnNames {
 
-    private MySqlColumnNames() {
-    }
-
     /**
-     * @param names column name
+     * @param names column names ordered by {@link #compare}
      * @param name  least 1 character enclosed by {@literal `} means it use case
      *              sensitive mode, otherwise use default mode (find with case
      *              sensitive first, then find with case insensitive when not
      *              found in case sensitive)
      * @return found index by special binary search, {@code -1} means not found.
      */
-    static int nameSearch(CharSequence[] names, String name) {
+    static int nameSearch(String[] names, String name) {
         int size = name.length();
-        if (size > 2 && name.charAt(0) == '`' && name.charAt(size - 1) == '`') {
-            return binarySearch(names, slice(name, 1, size - 1), false);
-        } else {
-            return binarySearch(names, name, true);
-        }
+        return binarySearch(names, name, size <= 2 || name.charAt(0) != '`' || name.charAt(size - 1) != '`');
     }
 
-    private static int binarySearch(CharSequence[] names, CharSequence name, boolean ignoreCase) {
-        int left = 0;
-        int right = names.length - 1;
+    private static int binarySearch(String[] names, String name, boolean ignoreCase) {
+        int left = 0, right = names.length - 1;
+        int nameStart = ignoreCase ? 0 : 1, nameEnd = ignoreCase ? name.length() : name.length() - 1;
         int ciResult = -1;
 
         while (left <= right) {
@@ -66,10 +58,10 @@ final class MySqlColumnNames {
             // `left + (right - left) / 2` = `(left + right) >>> 1`
             // when `left` and `right` is not negative integer.
             // And `left` must greater or equals than 0,
-            // `right` greater or equals than `left`.
+            // `right` greater then or equals to `left`.
             int middle = (left + right) >>> 1;
-            CharSequence value = names[middle];
-            int compared = compare(value, name);
+            String value = names[middle];
+            int compared = compare0(value, name, nameStart, nameEnd);
 
             if (compared < 0) {
                 left = middle + 1;
@@ -105,36 +97,40 @@ final class MySqlColumnNames {
      * <p>
      * Note: visible for unit tests.
      *
-     * @param left  the {@link CharSequence} of left
-     * @param right the {@link CharSequence} of right
+     * @param left  the {@link String} of left
+     * @param right the {@link String} of right
      * @return {@code 0} means both strings equals even case sensitive,
      * absolute value is {@code 2} means it is equals by case insensitive but not equals when case sensitive,
      * absolute value is {@code 4} means it is not equals even case insensitive.
      */
-    static int compare(CharSequence left, CharSequence right) {
-        int leftSize = left.length();
-        int rightSize = right.length();
+    static int compare(String left, String right) {
+        return compare0(left, right, 0, right.length());
+    }
+
+    private static int compare0(String left, String right, int start, int end) {
+        int leftSize = left.length(), rightSize = end - start;
         int minSize = Math.min(leftSize, rightSize);
         // Case sensitive comparator result.
         int csCompared = 0;
+        char leftCh, rightCh;
 
         for (int i = 0; i < minSize; i++) {
-            char leftChar = left.charAt(i);
-            char rightChar = right.charAt(i);
+            leftCh = left.charAt(i);
+            rightCh = right.charAt(i + start);
 
-            if (leftChar != rightChar) {
+            if (leftCh != rightCh) {
                 if (csCompared == 0) {
                     // Compare end if is case sensitive comparator.
-                    csCompared = leftChar - rightChar;
+                    csCompared = leftCh - rightCh;
                 }
 
                 // Use `Character.toLowerCase` for latin alpha.
-                leftChar = Character.toLowerCase(leftChar);
-                rightChar = Character.toLowerCase(rightChar);
+                leftCh = Character.toLowerCase(leftCh);
+                rightCh = Character.toLowerCase(rightCh);
 
-                if (leftChar != rightChar) {
+                if (leftCh != rightCh) {
                     // Not equals even case insensitive.
-                    return leftChar < rightChar ? -4 : 4;
+                    return leftCh < rightCh ? -4 : 4;
                 }
             }
         }
@@ -148,109 +144,6 @@ final class MySqlColumnNames {
         }
     }
 
-    /**
-     * Slice {@link String} for no copy with immutable.
-     * <p>
-     * WARNING: visible for unit tests, do NOT use it outer than {@link MySqlColumnNames}.
-     */
-    static CharSequence slice(String content, int start, int end) {
-        if (start < 0 || start > end || end > content.length()) {
-            throw new StringIndexOutOfBoundsException(String.format("Start index %d and end index %d out of range", start, end));
-        }
-
-        if (start == end) {
-            return Sliced.EMPTY;
-        } else {
-            return new Sliced(content, start, end);
-        }
-    }
-
-    private static final class Sliced implements CharSequence {
-
-        private static final Sliced EMPTY = new Sliced("", 0, 0);
-
-        private final String content;
-
-        private final int start;
-
-        private final int end;
-
-        private final int size;
-
-        private Sliced(String content, int start, int end) {
-            this.content = content;
-            this.start = start;
-            this.end = end;
-            this.size = end - start;
-        }
-
-        @Override
-        public int length() {
-            return size;
-        }
-
-        @Override
-        public char charAt(int index) {
-            if (index < 0 || index >= size) {
-                throw new StringIndexOutOfBoundsException(index);
-            }
-
-            return content.charAt(index + start);
-        }
-
-        @Override
-        public Sliced subSequence(int start, int end) {
-            if (start < 0 || start > end) {
-                throw new StringIndexOutOfBoundsException(start);
-            } else if (end > size) {
-                throw new StringIndexOutOfBoundsException(end);
-            }
-
-            if (start == end) {
-                return EMPTY;
-            } else {
-                return new Sliced(this.content, start + this.start, end + this.start);
-            }
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (!(o instanceof Sliced)) {
-                return false;
-            }
-
-            Sliced sliced = (Sliced) o;
-
-            if (size != sliced.size) {
-                return false;
-            }
-
-            for (int i = 0; i < size; ++i) {
-                if (content.charAt(i + start) != sliced.charAt(i + sliced.start)) {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
-        @Override
-        public int hashCode() {
-            int result = 0;
-
-            for (int i = 0; i < size; ++i) {
-                result = 31 * result + content.charAt(i + start);
-            }
-
-            return result;
-        }
-
-        @Override
-        public String toString() {
-            return content.substring(start, end);
-        }
+    private MySqlColumnNames() {
     }
 }
