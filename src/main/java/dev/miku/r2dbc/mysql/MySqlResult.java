a64ca3a612203267cfc52c1704c31d96058bf9a0
Mirro Mutth <mirromutth@gmail.com>
2019-11-11 18:32:01 +0800

    Recover discardOnCancel in MySqlResult

--- src/main/java/dev/miku/r2dbc/mysql/MySqlResult.java
+++ src/main/java/dev/miku/r2dbc/mysql/MySqlResult.java
@@ -25,7 +25,9 @@ import dev.miku.r2dbc.mysql.message.server.RowMessage;
 import dev.miku.r2dbc.mysql.message.server.ServerMessage;
 import dev.miku.r2dbc.mysql.message.server.SyntheticMetadataMessage;
 import dev.miku.r2dbc.mysql.util.ConnectionContext;
+import dev.miku.r2dbc.mysql.util.OperatorUtils;
 import io.netty.util.ReferenceCountUtil;
+import io.netty.util.ReferenceCounted;
 import io.r2dbc.spi.Result;
 import io.r2dbc.spi.Row;
 import io.r2dbc.spi.RowMetadata;
@@ -38,6 +40,7 @@ import reactor.util.annotation.Nullable;
 
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.BiFunction;
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 import static dev.miku.r2dbc.mysql.util.AssertUtils.requireNonNull;
@@ -48,6 +51,8 @@ import static dev.miku.r2dbc.mysql.util.AssertUtils.requireNonNull;
 public final class MySqlResult implements Result {
 
     private static final Function<OkMessage, Integer> ROWS_UPDATED = message -> (int) message.getAffectedRows();
+
+    private static final Consumer<ReferenceCounted> RELEASE = ReferenceCounted::release;
 
     private final boolean isBinary;
 
@@ -130,7 +135,7 @@ public final class MySqlResult implements Result {
             // Result mode, no need ok message.
             this.okProcessor.onComplete();
 
-            return messages;
+            return OperatorUtils.discardOnCancel(messages).doOnDiscard(ReferenceCounted.class, RELEASE);
         });
     }
 
