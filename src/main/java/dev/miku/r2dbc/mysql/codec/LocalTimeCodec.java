543d03c09ab77b7d31c9f1272a0d11983a6c4dc0
Mirro Mutth <mirromutth@gmail.com>
2020-06-13 14:01:09 +0800

    Polishing decoding of LocalTime

--- src/main/java/dev/miku/r2dbc/mysql/codec/LocalTimeCodec.java
+++ src/main/java/dev/miku/r2dbc/mysql/codec/LocalTimeCodec.java
@@ -32,6 +32,12 @@ import static dev.miku.r2dbc.mysql.codec.DateTimes.*;
  */
 final class LocalTimeCodec extends AbstractClassedCodec<LocalTime> {
 
+    private static final long NANOS_OF_DAY = ((long) SECONDS_OF_DAY) * NANOS_OF_SECOND;
+
+    private static final long NANOS_OF_HOUR = ((long) SECONDS_OF_HOUR) * NANOS_OF_SECOND;
+
+    private static final long NANOS_OF_MINUTE = ((long) SECONDS_OF_MINUTE) * NANOS_OF_SECOND;
+
     LocalTimeCodec(ByteBufAllocator allocator) {
         super(allocator, LocalTime.class);
     }
@@ -64,20 +70,16 @@ final class LocalTimeCodec extends AbstractClassedCodec<LocalTime> {
 
         if (buf.isReadable()) {
             int nano = readMicroInDigits(buf) * NANOS_OF_MICRO;
+
             if (isNegative) {
-                long totalNanos = -(((long) hour) * NANOS_OF_HOUR + ((long) minute) * NANOS_OF_MINUTE +
-                    ((long) second) * NANOS_OF_SECOND + ((long) nano));
-                return LocalTime.ofNanoOfDay(((totalNanos % NANOS_OF_DAY) + NANOS_OF_DAY) % NANOS_OF_DAY);
+                return negativeCircle(hour, minute, second, nano);
             } else {
                 return LocalTime.of(hour % HOURS_OF_DAY, minute, second, nano);
             }
         }
 
         if (isNegative) {
-            // The `hour` is a positive integer.
-            long totalSeconds = -(((long) hour) * SECONDS_OF_HOUR + ((long) minute) * SECONDS_OF_MINUTE +
-                ((long) second));
-            return LocalTime.ofSecondOfDay(((totalSeconds % SECONDS_OF_DAY) + SECONDS_OF_DAY) % SECONDS_OF_DAY);
+            return negativeCircle(hour, minute, second);
         } else {
             return LocalTime.of(hour % HOURS_OF_DAY, minute, second);
         }
@@ -114,29 +116,36 @@ final class LocalTimeCodec extends AbstractClassedCodec<LocalTime> {
 
         if (bytes < MICRO_TIME_SIZE) {
             if (isNegative) {
-                // The `hour` is a positive integer.
-                int totalSeconds = -(((int) hour) * SECONDS_OF_HOUR + ((int) minute) * SECONDS_OF_MINUTE + ((int) second));
-                return LocalTime.ofSecondOfDay(((totalSeconds % SECONDS_OF_DAY) + SECONDS_OF_DAY) % SECONDS_OF_DAY);
+                return negativeCircle(hour, minute, second);
             } else {
                 return LocalTime.of(hour % HOURS_OF_DAY, minute, second);
             }
         }
 
-        long micros = buf.readUnsignedIntLE();
+        long nano = buf.readUnsignedIntLE() * NANOS_OF_MICRO;
 
         if (isNegative) {
-            long nanos = -(((long) hour) * NANOS_OF_HOUR + ((long) minute) * NANOS_OF_MINUTE +
-                ((long) second) * NANOS_OF_SECOND + micros * NANOS_OF_MICRO);
-
-            return LocalTime.ofNanoOfDay(((nanos % NANOS_OF_DAY) + NANOS_OF_DAY) % NANOS_OF_DAY);
+            return negativeCircle(hour, minute, second, nano);
         } else {
-            return LocalTime.of(hour % HOURS_OF_DAY, minute, second, (int) (micros * NANOS_OF_MICRO));
+            return LocalTime.of(hour % HOURS_OF_DAY, minute, second, (int) nano);
         }
     }
 
     static void encodeTime(ParameterWriter writer, LocalTime time) {
         int micros = time.getNano() / NANOS_OF_MICRO;
         DurationCodec.encodeTime(writer, false, time.getHour(), time.getMinute(), time.getSecond(), micros);
+    }
+
+    private static LocalTime negativeCircle(int hour, int minute, int second) {
+        int total = -(hour * SECONDS_OF_HOUR + minute * SECONDS_OF_MINUTE + second);
+        return LocalTime.ofSecondOfDay(((total % SECONDS_OF_DAY) + SECONDS_OF_DAY) % SECONDS_OF_DAY);
+    }
+
+    private static LocalTime negativeCircle(long hour, long minute, long second, long nano) {
+        long total = -(hour * NANOS_OF_HOUR + minute * NANOS_OF_MINUTE +
+            second * NANOS_OF_SECOND + nano);
+
+        return LocalTime.ofNanoOfDay(((total % NANOS_OF_DAY) + NANOS_OF_DAY) % NANOS_OF_DAY);
     }
 
     private static final class LocalTimeParameter extends AbstractParameter {
