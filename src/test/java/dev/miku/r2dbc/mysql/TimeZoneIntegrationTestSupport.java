6c34ce92ffe93a8938191b528ff1b51a556aeb6a
Mirro Mutth <mirromutth@gmail.com>
2020-06-27 18:12:24 +0800

    Add integration tests for time zone

--- src/test/java/dev/miku/r2dbc/mysql/TimeZoneIntegrationTestSupport.java
+++ src/test/java/dev/miku/r2dbc/mysql/TimeZoneIntegrationTestSupport.java
@@ -22,8 +22,15 @@ import reactor.util.annotation.Nullable;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.OffsetDateTime;
+import java.time.OffsetTime;
 import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.TimeZone;
 import java.util.function.Predicate;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -33,55 +40,182 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 abstract class TimeZoneIntegrationTestSupport extends IntegrationTestSupport {
 
-    private static final ZoneId SERVER_ZONE_ID = ZoneId.of("GMT+6");
+    private static final String TIMESTAMP_TABLE = "CREATE TEMPORARY TABLE test (id INT PRIMARY KEY AUTO_INCREMENT, value TIMESTAMP)";
+
+    private static final String TIME_TABLE = "CREATE TEMPORARY TABLE test (id INT PRIMARY KEY AUTO_INCREMENT, value TIME)";
+
+    private static final LocalDateTime ST = LocalDateTime.of(2019, 1, 1, 11, 59, 59);
+
+    private static final LocalDateTime DST = ST.plusHours(200 * 24);
+
+    private static final String INSERT = String.format("INSERT INTO test VALUE (DEFAULT, '%s'), (DEFAULT, '%s')",
+        DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(ST),
+        DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(DST));
+
+    private static final ZoneId SERVER_ZONE = ZoneId.of("America/New_York");
 
     static {
-        // This timezone is just a time zone for tests.
-        System.setProperty("user.timezone", "GMT+2");
+        TimeZone.setDefault(TimeZone.getTimeZone("GMT+6"));
+
+        // Make sure test cases contains daylight.
+        assertThat(ST.atZone(SERVER_ZONE).plusHours(200 * 24))
+            .isEqualTo(DST.atZone(SERVER_ZONE).plusHours(1));
     }
 
     TimeZoneIntegrationTestSupport(@Nullable Predicate<String> preferPrepared) {
-        // This timezone is just a time zone for tests.
-        super(configuration(false, SERVER_ZONE_ID, preferPrepared));
+        super(configuration(false, SERVER_ZONE, preferPrepared));
+    }
+
+    @Test
+    void queryZonedDateTime() {
+        complete(connection -> Flux.from(connection.createStatement(TIMESTAMP_TABLE).execute())
+            .flatMap(IntegrationTestSupport::extractRowsUpdated)
+            .thenMany(connection.createStatement(INSERT).execute())
+            .flatMap(IntegrationTestSupport::extractRowsUpdated)
+            .thenMany(connection.createStatement("SELECT value FROM test WHERE id > ?")
+                .bind(0, 0)
+                .execute())
+            .flatMap(r -> r.map((row, meta) -> row.get(0, ZonedDateTime.class)))
+            .doOnNext(it -> assertThat(it.getZone()).isEqualTo(SERVER_ZONE))
+            .map(ZonedDateTime::toLocalDateTime)
+            .collectList()
+            .doOnNext(it -> assertThat(it).isEqualTo(Arrays.asList(ST, DST))));
+    }
+
+    @Test
+    void queryOffsetDateTime() {
+        complete(connection -> Flux.from(connection.createStatement(TIMESTAMP_TABLE).execute())
+            .flatMap(IntegrationTestSupport::extractRowsUpdated)
+            .thenMany(connection.createStatement(INSERT).execute())
+            .flatMap(IntegrationTestSupport::extractRowsUpdated)
+            .thenMany(connection.createStatement("SELECT value FROM test WHERE id > ?")
+                .bind(0, 0)
+                .execute())
+            .flatMap(r -> r.map((row, meta) -> row.get(0, OffsetDateTime.class)))
+            .doOnNext(it -> assertThat(it.getOffset()).isEqualTo(SERVER_ZONE.getRules().getOffset(it.toLocalDateTime())))
+            .map(OffsetDateTime::toLocalDateTime)
+            .collectList()
+            .doOnNext(it -> assertThat(it).isEqualTo(Arrays.asList(ST, DST))));
     }
 
     @Test
     void queryInstant() {
-        LocalDateTime serverLocal = LocalDateTime.of(2020, 6, 17, 15, 0, 0);
-        String serverTime = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(serverLocal);
-        complete(connection -> Flux.from(connection.createStatement("CREATE TEMPORARY TABLE test (id INT PRIMARY KEY AUTO_INCREMENT, value TIMESTAMP)")
-            .execute())
+        complete(connection -> Flux.from(connection.createStatement(TIMESTAMP_TABLE).execute())
             .flatMap(IntegrationTestSupport::extractRowsUpdated)
-            .thenMany(connection.createStatement(String.format("INSERT INTO test VALUE (DEFAULT, '%s')", serverTime))
-                .execute())
+            .thenMany(connection.createStatement(INSERT).execute())
             .flatMap(IntegrationTestSupport::extractRowsUpdated)
-            .thenMany(connection.createStatement("SELECT value FROM test WHERE id = ?")
-                .bind(0, 1)
+            .thenMany(connection.createStatement("SELECT value FROM test WHERE id > ?")
+                .bind(0, 0)
                 .execute())
             .flatMap(r -> r.map((row, meta) -> row.get(0, Instant.class)))
-            .map(it -> LocalDateTime.ofInstant(it, SERVER_ZONE_ID))
-            .doOnNext(it -> assertThat(it).isEqualTo(serverLocal)));
+            .map(it -> LocalDateTime.ofInstant(it, SERVER_ZONE))
+            .collectList()
+            .doOnNext(it -> assertThat(it).isEqualTo(Arrays.asList(ST, DST))));
+    }
+
+    @Test
+    void queryOffsetTime() {
+        complete(connection -> Flux.from(connection.createStatement(TIME_TABLE).execute())
+            .thenMany(connection.createStatement("INSERT INTO test VALUE (DEFAULT, '11:23:58')")
+                .execute())
+            .flatMap(IntegrationTestSupport::extractRowsUpdated)
+            .thenMany(connection.createStatement("SELECT value FROM test WHERE id > ?")
+                .bind(0, 0)
+                .execute())
+            .flatMap(r -> r.map((row, meta) -> row.get(0, OffsetTime.class)))
+            .doOnNext(it -> assertThat(it.getOffset()).isEqualTo(SERVER_ZONE.getRules().getStandardOffset(Instant.EPOCH)))
+            .map(OffsetTime::toLocalTime)
+            .collectList()
+            .doOnNext(it -> assertThat(it)
+                .isEqualTo(Collections.singletonList(LocalTime.of(11, 23, 58)))));
+    }
+
+    @Test
+    void updateZonedDateTime() {
+        complete(connection -> Flux.from(connection.createStatement(TIMESTAMP_TABLE).execute())
+            .flatMap(IntegrationTestSupport::extractRowsUpdated)
+            .thenMany(connection.createStatement("INSERT INTO test VALUE (DEFAULT, ?)")
+                .bind(0, toZonedDateTime(ST))
+                .add()
+                .bind(0, toZonedDateTime(DST))
+                .execute())
+            .flatMap(IntegrationTestSupport::extractRowsUpdated)
+            .thenMany(connection.createStatement("SELECT value FROM test WHERE id > ?")
+                .bind(0, 0)
+                .execute())
+            .flatMap(r -> r.map((row, meta) -> row.get(0, ZonedDateTime.class)))
+            .doOnNext(it -> assertThat(it.getZone()).isEqualTo(SERVER_ZONE))
+            .map(it -> it.withZoneSameInstant(ZoneId.systemDefault()).toLocalDateTime())
+            .collectList()
+            .doOnNext(it -> assertThat(it).isEqualTo(Arrays.asList(ST, DST))));
+    }
+
+    @Test
+    void updateOffsetDateTime() {
+        complete(connection -> Flux.from(connection.createStatement(TIMESTAMP_TABLE).execute())
+            .flatMap(IntegrationTestSupport::extractRowsUpdated)
+            .thenMany(connection.createStatement("INSERT INTO test VALUE (DEFAULT, ?)")
+                .bind(0, toOffsetDateTime(ST))
+                .add()
+                .bind(0, toOffsetDateTime(DST))
+                .execute())
+            .flatMap(IntegrationTestSupport::extractRowsUpdated)
+            .thenMany(connection.createStatement("SELECT value FROM test WHERE id > ?")
+                .bind(0, 0)
+                .execute())
+            .flatMap(r -> r.map((row, meta) -> row.get(0, OffsetDateTime.class)))
+            .doOnNext(it -> assertThat(it.getOffset()).isEqualTo(SERVER_ZONE.getRules().getOffset(it.toLocalDateTime())))
+            .map(it -> it.toZonedDateTime().withZoneSameInstant(ZoneId.systemDefault()).toLocalDateTime())
+            .collectList()
+            .doOnNext(it -> assertThat(it).isEqualTo(Arrays.asList(ST, DST))));
     }
 
     @Test
     void updateInstant() {
-        LocalDateTime clientLocal = LocalDateTime.of(2020, 6, 17, 15, 0, 0);
-        complete(connection -> Flux.from(connection.createStatement("CREATE TEMPORARY TABLE test (id INT PRIMARY KEY AUTO_INCREMENT, value TIMESTAMP)")
-            .execute())
+        complete(connection -> Flux.from(connection.createStatement(TIMESTAMP_TABLE).execute())
             .flatMap(IntegrationTestSupport::extractRowsUpdated)
             .thenMany(connection.createStatement("INSERT INTO test VALUE (DEFAULT, ?)")
-                .bind(0, toInstant(clientLocal))
+                .bind(0, toInstant(ST))
+                .add()
+                .bind(0, toInstant(DST))
                 .execute())
             .flatMap(IntegrationTestSupport::extractRowsUpdated)
-            .thenMany(connection.createStatement("SELECT value FROM test WHERE id = ?")
-                .bind(0, 1)
+            .thenMany(connection.createStatement("SELECT value FROM test WHERE id > ?")
+                .bind(0, 0)
                 .execute())
             .flatMap(r -> r.map((row, meta) -> row.get(0, Instant.class)))
             .map(it -> LocalDateTime.ofInstant(it, ZoneId.systemDefault()))
-            .doOnNext(it -> assertThat(it).isEqualTo(clientLocal)));
+            .collectList()
+            .doOnNext(it -> assertThat(it).isEqualTo(Arrays.asList(ST, DST))));
+    }
+
+    @Test
+    void updateOffsetTime() {
+        complete(connection -> Flux.from(connection.createStatement(TIME_TABLE).execute())
+            .thenMany(connection.createStatement("INSERT INTO test VALUE (DEFAULT, ?)")
+                .bind(0, OffsetTime.of(11, 23, 58, 0, ZoneId.systemDefault().getRules().getStandardOffset(Instant.EPOCH)))
+                .execute())
+            .flatMap(IntegrationTestSupport::extractRowsUpdated)
+            .thenMany(connection.createStatement("SELECT value FROM test WHERE id > ?")
+                .bind(0, 0)
+                .execute())
+            .flatMap(r -> r.map((row, meta) -> row.get(0, OffsetTime.class)))
+            .doOnNext(it -> assertThat(it.getOffset()).isEqualTo(SERVER_ZONE.getRules().getStandardOffset(Instant.EPOCH)))
+            .map(it -> it.withOffsetSameInstant(ZoneId.systemDefault().getRules().getStandardOffset(Instant.EPOCH)).toLocalTime())
+            .collectList()
+            .doOnNext(it -> assertThat(it)
+                .isEqualTo(Collections.singletonList(LocalTime.of(11, 23, 58)))));
     }
 
     private static Instant toInstant(LocalDateTime value) {
         return value.toInstant(ZoneId.systemDefault().getRules().getOffset(value));
+    }
+
+    private static ZonedDateTime toZonedDateTime(LocalDateTime value) {
+        return value.atZone(ZoneId.systemDefault());
+    }
+
+    private static OffsetDateTime toOffsetDateTime(LocalDateTime value) {
+        return value.atOffset(ZoneId.systemDefault().getRules().getOffset(value));
     }
 }
