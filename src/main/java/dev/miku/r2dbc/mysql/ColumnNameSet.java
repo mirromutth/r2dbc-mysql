84f2ac848afa029ce14b94a4fe0255a6bcd9661c
Mirro Mutth <mirromutth@gmail.com>
2020-12-15 23:12:15 +0800

    Correct iteration order of ColumnNameSet

--- src/main/java/dev/miku/r2dbc/mysql/ColumnNameSet.java
+++ src/main/java/dev/miku/r2dbc/mysql/ColumnNameSet.java
@@ -29,6 +29,9 @@ import java.util.Spliterators;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
+import static dev.miku.r2dbc.mysql.util.AssertUtils.require;
+import static dev.miku.r2dbc.mysql.util.AssertUtils.requireNonNull;
+
 /**
  * An implementation of {@link Set}{@code <}{@link String}{@code >} for {@code RowMetadata.getColumnNames} results.
  *
@@ -36,21 +39,25 @@ import java.util.function.Predicate;
  */
 final class ColumnNameSet extends AbstractSet<String> implements Set<String> {
 
-    private final String[] names;
+    private final String[] originNames;
+
+    private final String[] sortedNames;
 
     /**
      * Construct a {@link ColumnNameSet} by sorted {@code names} without array copy.
      *
-     * @param names must be sorted by {@link MySqlNames#compare(String, String)}.
+     * @param originNames must be the original order.
+     * @param sortedNames must be sorted by {@link MySqlNames#compare(String, String)}.
      */
-    ColumnNameSet(String... names) {
-        this.names = names;
+    private ColumnNameSet(String[] originNames, String[] sortedNames) {
+        this.originNames = originNames;
+        this.sortedNames = sortedNames;
     }
 
     @Override
     public boolean contains(Object o) {
         if (o instanceof String) {
-            return MySqlNames.nameSearch(this.names, (String) o) >= 0;
+            return MySqlNames.nameSearch(this.sortedNames, (String) o) >= 0;
         }
 
         return false;
@@ -58,36 +65,36 @@ final class ColumnNameSet extends AbstractSet<String> implements Set<String> {
 
     @Override
     public Iterator<String> iterator() {
-        return InternalArrays.asIterator(names);
+        return InternalArrays.asIterator(originNames);
     }
 
     @Override
     public int size() {
-        return names.length;
+        return originNames.length;
     }
 
     @Override
     public boolean isEmpty() {
-        return names.length == 0;
+        return originNames.length == 0;
     }
 
     @Override
     public Spliterator<String> spliterator() {
-        return Spliterators.spliterator(this.names, Spliterator.NONNULL | Spliterator.ORDERED | Spliterator.IMMUTABLE);
+        return Spliterators.spliterator(this.originNames, Spliterator.NONNULL | Spliterator.ORDERED | Spliterator.IMMUTABLE);
     }
 
     @Override
     public void forEach(Consumer<? super String> action) {
         Objects.requireNonNull(action);
 
-        for (String name : this.names) {
+        for (String name : this.originNames) {
             action.accept(name);
         }
     }
 
     @Override
     public String[] toArray() {
-        return Arrays.copyOf(names, names.length);
+        return Arrays.copyOf(originNames, originNames.length);
     }
 
     @SuppressWarnings({"unchecked", "SuspiciousSystemArraycopy"})
@@ -95,12 +102,12 @@ final class ColumnNameSet extends AbstractSet<String> implements Set<String> {
     public <T> T[] toArray(T[] a) {
         Objects.requireNonNull(a);
 
-        int size = names.length;
+        int size = originNames.length;
 
         if (a.length < size) {
-            return (T[]) Arrays.copyOf(names, size, a.getClass());
+            return (T[]) Arrays.copyOf(originNames, size, a.getClass());
         } else {
-            System.arraycopy(names, 0, a, 0, size);
+            System.arraycopy(originNames, 0, a, 0, size);
 
             if (a.length > size) {
                 a[size] = null;
@@ -135,7 +142,7 @@ final class ColumnNameSet extends AbstractSet<String> implements Set<String> {
     public boolean removeIf(Predicate<? super String> filter) {
         Objects.requireNonNull(filter);
 
-        for (String name : this.names) {
+        for (String name : this.originNames) {
             if (filter.test(name)) {
                 throw new UnsupportedOperationException();
             }
@@ -173,6 +180,21 @@ final class ColumnNameSet extends AbstractSet<String> implements Set<String> {
 
     @Override
     public String toString() {
-        return Arrays.toString(names);
+        return Arrays.toString(originNames);
+    }
+
+    static ColumnNameSet of(String name) {
+        requireNonNull(name, "name must not be null");
+
+        String[] names = new String[]{name};
+        return new ColumnNameSet(names, names);
+    }
+
+    static ColumnNameSet of(String[] originNames, String[] sortedNames) {
+        requireNonNull(originNames, "originNames must not be null");
+        requireNonNull(sortedNames, "sortedNames must not be null");
+        require(originNames.length == sortedNames.length, "The length of origin names the same as sorted names one");
+
+        return new ColumnNameSet(originNames, sortedNames);
     }
 }
